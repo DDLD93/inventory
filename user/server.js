c6ce8d6b5cdcc907dc8afb671283cd0781ebfb80
ddld93 <umar.jere@gmail.com>
2022-04-05 14:40:33 +0100

    commit

--- user/server.js
+++ user/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express()
-const port = process.env.PORT || 6666
+const port = process.env.PORT || 6600
 
 
 app.use(cors());
